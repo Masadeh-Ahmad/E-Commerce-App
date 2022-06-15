8517ce6a87a2ba7ca0c5cd5b873f98dc727d9391
Ahmad Masadeh <62568250+MasadehAhmad@users.noreply.github.com>
2022-06-16 00:04:10 +0300

    cart

---
+++ E-Commerce-App/E-Commerce-App/Components/CartCount.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace E_Commerce_App.Components
+{
+    public class CartCount : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            ViewComponentModel cartData = new ViewComponentModel { Name = HttpContext.Request.Cookies["username"] };
+            return View(cartData);
+        }
+
+        public class ViewComponentModel
+        {
+            public string Name { get; set; }
+        }
+    }
+}
