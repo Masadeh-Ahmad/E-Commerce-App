86fc603e17ea54c1eb4d38b204fbc12c2464212c
Ahmad Masadeh <62568250+MasadehAhmad@users.noreply.github.com>
2022-06-21 01:53:45 +0300

    add ViewComponent

--- E-Commerce-App/E-Commerce-App/Pages/Auth1/Index.cshtml.cs
+++ E-Commerce-App/E-Commerce-App/Pages/Auth1/Index.cshtml.cs
@@ -1,7 +1,9 @@
 using E_Commerce_App.Auth.Interfaces;
 using E_Commerce_App.Auth.Models.DTO;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 using System.Threading.Tasks;
 
 namespace E_Commerce_App.Pages.Accounts
@@ -9,16 +11,23 @@ namespace E_Commerce_App.Pages.Accounts
     public class IndexModel : PageModel
     {
         private IUserService userService;
-
+        [BindProperty]
+        public string UserName { get; set; }
+        [BindProperty]
+        public string Password { get; set; }
         public IndexModel(IUserService userSer)
         {
             userService = userSer;
         }
-        public async Task OnGet(LoginDTO login)
+        public async Task OnPostAsync()
         {
-            var user = await userService.Authenticate(login.UserName, login.Password);
-          
-           
+            await userService.Authenticate(UserName, Password);
+            
+            CookieOptions cookieOptions = new CookieOptions();
+            cookieOptions.Expires = new System.DateTimeOffset(DateTime.Now.AddDays(7));
+            HttpContext.Response.Cookies.Append("username", UserName, cookieOptions);
+
+
         }
     }
 }
---
+++ E-Commerce-App/E-Commerce-App/Pages/Shared/Components/CartCount/Default.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace E_Commerce_App.Pages.Components
+{
+    public class DefaultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
